2da264553e660eab7a90969335f6afe0a5ddecce
13thrtn <rusinovich.1998@mail.ru>
2023-01-21 15:04:32 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,197 @@
+'use strict'
+
+$(document).ready(function () {
+  // Header menu mob
+  $('.header__list')
+    .parent()
+    .find('a')
+    .click(function (e) {
+      if ($(window).width() <= 768) {
+        e.preventDefault()
+      }
+    })
+  $('.header__menu-item').click(function () {
+    if ($(window).width() <= 768) {
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active').find('.header__list').slideUp()
+      } else {
+        $('.header__menu-item.active')
+          .removeClass('active')
+          .find('.header__list')
+          .slideUp()
+        $(this).toggleClass('active').find('.header__list').slideToggle()
+      }
+    }
+  })
+  $('.header__language-btn').click(function () {
+    if ($(window).width() <= 768) {
+      $(this).toggleClass('active')
+      $('.header__language-list').slideToggle()
+    }
+  })
+  $('.header__close-btn, .header__burger-bg').click(function () {
+    $('body').removeClass('lock')
+    $('.header__burger-bg').fadeOut()
+    $('.header__content').removeClass('active')
+  })
+  $('.header__burger').click(function () {
+    $('body').addClass('lock')
+    $('.header__burger-bg').fadeIn()
+    $('.header__content').addClass('active')
+  })
+
+  // Catalog
+  $('.catalog__filter-label input').change(function () {
+    $(this).parent().toggleClass('active')
+  })
+  $('.catalog__tab input').change(function () {
+    $(this).parent().siblings('.catalog__tab').removeClass('active')
+    $(this).parent().addClass('active')
+  })
+
+  // Contacts
+  $('.contacts__label input, .contacts__label-textarea textarea')
+    .focusin(function () {
+      $(this).parent().addClass('focus')
+    })
+    .focusout(function () {
+      $(this).parent().removeClass('focus')
+    })
+
+  // Modal
+  // Request
+  $('.modal-request__form').iziModal({
+    overlayColor: 'rgba(0, 3, 21, 0.5)',
+    width: '108rem',
+    zindex: 30,
+    group: 'request',
+    transitionIn: 'fadeInUp',
+    transitionOut: 'comingOut',
+    navigateArrows: false,
+    navigateCaption: false,
+    onOpening: function () {
+      $('body').width($('body').width()).addClass('lock')
+    },
+    onClosing: function () {
+      $('body').width('').removeClass('lock')
+    },
+  })
+  $('.modal-request__complete').iziModal({
+    overlayColor: 'rgba(0, 3, 21, 0.5)',
+    width: '69rem',
+    zindex: 30,
+    group: 'request',
+    transitionIn: 'fadeInUp',
+    transitionOut: 'comingOut',
+    navigateArrows: false,
+    navigateCaption: false,
+    onOpening: function () {
+      $('body').width($('body').width()).addClass('lock')
+    },
+    onClosing: function () {
+      $('body').width('').removeClass('lock')
+    },
+  })
+  $('.popup-request').click(function () {
+    $('.modal-request__form').iziModal('open')
+  })
+  $('.popup-request-complete').click(function () {
+    $('.modal-request__complete').iziModal('open')
+  })
+  $('.modal-request__label input, .modal-request__label-textarea textarea')
+    .focusin(function () {
+      $(this).parent().addClass('focus')
+    })
+    .focusout(function () {
+      $(this).parent().removeClass('focus')
+    })
+
+  // Modal
+  // Work1
+  $('.modal-work1').iziModal({
+    overlayColor: 'rgba(0, 3, 21, 0.5)',
+    width: '108rem',
+    zindex: 30,
+    transitionIn: 'fadeInUp',
+    transitionOut: 'comingOut',
+    navigateArrows: false,
+    navigateCaption: false,
+    onOpening: function () {
+      $('body').width($('body').width()).addClass('lock')
+    },
+    onClosing: function () {
+      $('body').width('').removeClass('lock')
+    },
+  })
+  $('.popup-work1').click(function () {
+    $('.modal-work1').iziModal('open')
+  })
+
+  // Modal
+  // Work2
+  $('.modal-work2').iziModal({
+    overlayColor: 'rgba(0, 3, 21, 0.5)',
+    width: '108rem',
+    zindex: 30,
+    transitionIn: 'fadeInUp',
+    transitionOut: 'comingOut',
+    navigateArrows: false,
+    navigateCaption: false,
+    onOpening: function () {
+      $('body').width($('body').width()).addClass('lock')
+    },
+    onClosing: function () {
+      $('body').width('').removeClass('lock')
+    },
+  })
+  $('.popup-work2').click(function () {
+    $('.modal-work2').iziModal('open')
+  })
+
+  // Modal
+  // Licenses
+  $('.popup-licenses').click(function () {
+    $('.modal-licenses').iziModal('open')
+  })
+  $('.licenses__slide').click(function () {
+    modal_licenses_slider.slideToLoop($(this).index())
+    $('.modal-licenses').iziModal('open')
+  })
+
+  // Modal
+  // Realized
+  $('.popup-realized').click(function () {
+    $('.modal-realized').iziModal('open')
+  })
+  $('.realized__img-box').click(function () {
+    modal_realized_slider.slideToLoop($(this).index())
+    $('.modal-realized').iziModal('open')
+  })
+
+  // ========= by Olesya ===========
+
+  // Article
+  $('.article__tab input').change(function () {
+    $(this).parent().siblings('.article__tab').removeClass('active')
+    $(this).parent().addClass('active')
+  })
+
+  // Product-info
+  $('.product-info__tab input').change(function () {
+    $(this).parent().siblings('.product-info__tab').removeClass('active')
+    $(this).parent().addClass('active')
+  })
+
+  document.querySelectorAll('.product-info__tab').forEach(function (tabBtn) {
+    tabBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path
+
+      document
+        .querySelectorAll('.product-info__panel')
+        .forEach(function (tabContent) {
+          tabContent.classList.remove('active')
+        })
+      document.querySelector(`[data-target="${path}"]`).classList.add('active')
+    })
+  })
+})
---
+++ js/swiper.js
@@ -0,0 +1,496 @@
+// Пересчет rem в px для swiper
+const rem = function (rem) {
+  if ($(window).width() > 768) {
+    return 0.005208335 * $(window).width() * rem
+  } else {
+    // где 375 это ширина моб версии макета
+    return (100 / 375) * (0.1 * $(window).width()) * rem
+  }
+}
+
+const main_intro_slider = new Swiper('.main-intro__slider', {
+  direction: 'horizontal',
+  centeredSlides: true,
+  grabCursor: true,
+  speed: 500,
+  effect: 'creative',
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.31,
+
+      pagination: {
+        el: '.main-intro .slider-nav__pagination.mobile',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '"></span>'
+        },
+      },
+
+      creativeEffect: {
+        prev: {
+          translate: [rem(-7), 0, 0],
+          scale: [0.8],
+        },
+        next: {
+          translate: [rem(7), 0, 0],
+          scale: [0.8],
+        },
+      },
+    },
+    769: {
+      slidesPerView: 1.53,
+
+      pagination: {
+        el: '.main-intro .slider-nav__pagination.desktop',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '"></span>'
+        },
+      },
+
+      creativeEffect: {
+        prev: {
+          translate: [rem(-15), 0, 0],
+          scale: [0.75],
+        },
+        next: {
+          translate: [rem(15), 0, 0],
+          scale: [0.75],
+        },
+      },
+    },
+  },
+
+  navigation: {
+    nextEl: '.main-intro .next',
+    prevEl: '.main-intro .prev',
+  },
+
+  autoplay: {
+    delay: 7000,
+  },
+})
+
+const sales_leader_slider = new Swiper('.sales-leader__slider', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: rem(3),
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    769: {
+      slidesPerView: 4,
+    },
+  },
+
+  navigation: {
+    nextEl: '.sales-leader .next',
+    prevEl: '.sales-leader .prev',
+  },
+
+  autoplay: {
+    delay: 12000,
+  },
+})
+
+const services_slider = new Swiper('.services__slider', {
+  direction: 'horizontal',
+  spaceBetween: rem(3),
+  slidesPerView: 1,
+  speed: 500,
+  effect: 'creative',
+  loop: true,
+
+  creativeEffect: {
+    prev: {
+      opacity: 0,
+    },
+    next: {
+      opacity: 0,
+    },
+  },
+
+  pagination: {
+    el: '.services__pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 12000,
+  },
+
+  on: {
+    slideChange: function (slider) {
+      $('.services__tab').removeClass('active')
+      $('.services__tab:nth-child(' + (slider.realIndex + 1) + ')').addClass(
+        'active'
+      )
+    },
+  },
+})
+
+$('.services__tab').click(function () {
+  services_slider.slideTo($(this).index() + 1)
+})
+
+const partners_slider = new Swiper('.partners__slider', {
+  direction: 'horizontal',
+  loop: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.454,
+      spaceBetween: rem(2),
+    },
+    769: {
+      slidesPerView: 4,
+      spaceBetween: rem(3),
+    },
+  },
+
+  navigation: {
+    nextEl: '.partners .next',
+    prevEl: '.partners .prev',
+  },
+
+  autoplay: {
+    delay: 12000,
+  },
+})
+
+const realized_slider = new Swiper('.realized-slider__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  speed: 500,
+  effect: 'creative',
+
+  creativeEffect: {
+    prev: {
+      opacity: 0,
+      translate: [rem(4), 0, 0],
+    },
+    next: {
+      opacity: 0,
+      translate: [rem(4), 0, 0],
+    },
+  },
+
+  navigation: {
+    nextEl: '.realized-slider .next',
+    prevEl: '.realized-slider .prev',
+  },
+})
+
+const licenses_slider = new Swiper('.licenses__slider', {
+  direction: 'horizontal',
+  spaceBetween: rem(3),
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    769: {
+      slidesPerView: 4,
+    },
+  },
+
+  navigation: {
+    nextEl: '.licenses .next',
+    prevEl: '.licenses .prev',
+  },
+})
+
+$('.modal-licenses').iziModal({
+  overlayColor: 'rgba(0, 3, 21, 0.5)',
+  width: '100%',
+  zindex: 30,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'comingOut',
+  navigateArrows: false,
+  navigateCaption: false,
+  onOpening: function () {
+    $('body').width($('body').width()).addClass('lock')
+  },
+  onClosing: function () {
+    $('body').width('').removeClass('lock')
+  },
+})
+
+const modal_licenses_slider = new Swiper('.modal-licenses__slider', {
+  direction: 'horizontal',
+  spaceBetween: rem(4),
+  centeredSlides: true,
+  loop: true,
+  grabCursor: true,
+  speed: 700,
+  effect: 'creative',
+
+  creativeEffect: {
+    limitProgress: 3,
+    prev: {
+      translate: ['-100%', 0, 0],
+      scale: 0.85,
+    },
+    next: {
+      translate: ['100%', 0, 0],
+      scale: 0.85,
+    },
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    769: {
+      slidesPerView: 2.6,
+    },
+  },
+
+  pagination: {
+    el: '.modal-licenses .page-pagination__numbers',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<span class="page-pagination__link ' +
+        className +
+        '"><span>' +
+        (index + 1) +
+        '</span></span>'
+      )
+    },
+  },
+
+  navigation: {
+    nextEl: '.modal-licenses .next',
+    prevEl: '.modal-licenses .prev',
+  },
+})
+
+const catalog_intro_slider = new Swiper('.catalog-intro__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  speed: 700,
+  effect: 'creative',
+
+  creativeEffect: {
+    prev: {
+      opacity: 0,
+      translate: [rem(4), 0, 0],
+    },
+    next: {
+      opacity: 0,
+      translate: [rem(4), 0, 0],
+    },
+  },
+
+  breakpoints: {
+    0: {
+      pagination: {
+        el: '.catalog-intro .slider-nav__pagination.mobile',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="page-pagination__link ' + className + '"></span>'
+        },
+      },
+    },
+    769: {
+      pagination: {
+        el: '.catalog-intro .slider-nav__pagination.desktop',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="page-pagination__link ' + className + '"></span>'
+        },
+      },
+    },
+  },
+
+  navigation: {
+    nextEl: '.catalog-intro .next',
+    prevEl: '.catalog-intro .prev',
+  },
+
+  autoplay: {
+    delay: 15000,
+  },
+})
+
+$('.modal-realized').iziModal({
+  overlayColor: 'rgba(0, 3, 21, 0.5)',
+  width: '100%',
+  zindex: 30,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'comingOut',
+  navigateArrows: false,
+  navigateCaption: false,
+  onOpening: function () {
+    $('body').width($('body').width()).addClass('lock')
+  },
+  onClosing: function () {
+    $('body').width('').removeClass('lock')
+  },
+})
+
+const modal_realized_slider = new Swiper('.modal-realized__slider', {
+  direction: 'horizontal',
+  spaceBetween: rem(4),
+  centeredSlides: true,
+  loop: true,
+  grabCursor: true,
+  speed: 700,
+  effect: 'creative',
+
+  creativeEffect: {
+    limitProgress: 3,
+    prev: {
+      translate: ['-100%', 0, 0],
+      scale: 0.85,
+    },
+    next: {
+      translate: ['100%', 0, 0],
+      scale: 0.85,
+    },
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    769: {
+      slidesPerView: 'auto',
+    },
+  },
+
+  pagination: {
+    el: '.modal-realized .page-pagination__numbers',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<span class="page-pagination__link ' +
+        className +
+        '"><span>' +
+        (index + 1) +
+        '</span></span>'
+      )
+    },
+  },
+
+  navigation: {
+    nextEl: '.modal-realized .next',
+    prevEl: '.modal-realized .prev',
+  },
+})
+
+const about_intro_slider = new Swiper('.about-intro__slider', {
+  direction: 'horizontal',
+  spaceBetween: rem(3),
+  loop: true,
+  speed: 700,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+      spaceBetween: rem(2),
+
+      pagination: {
+        el: '.about-intro .slider-nav__pagination.mobile',
+        type: 'bullets',
+        clickable: true,
+      },
+    },
+    769: {
+      slidesPerView: 3,
+      spaceBetween: rem(3),
+
+      pagination: {
+        el: '.about-intro .slider-nav__pagination.desktop',
+        type: 'bullets',
+        clickable: true,
+      },
+    },
+  },
+
+  navigation: {
+    nextEl: '.about-intro .next',
+    prevEl: '.about-intro .prev',
+  },
+})
+
+const work_intro_slider = new Swiper('.work-intro__slider', {
+  direction: 'horizontal',
+  spaceBetween: rem(3),
+  loop: true,
+  speed: 700,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: rem(2),
+
+      pagination: {
+        el: '.work-intro .slider-nav__pagination.mobile',
+        type: 'bullets',
+        clickable: true,
+      },
+    },
+    769: {
+      slidesPerView: 2.41,
+      spaceBetween: rem(3),
+
+      pagination: {
+        el: '.work-intro .slider-nav__pagination.desktop',
+        type: 'bullets',
+        clickable: true,
+      },
+    },
+  },
+
+  navigation: {
+    nextEl: '.work-intro .next',
+    prevEl: '.work-intro .prev',
+  },
+})
+
+const stages_slider = new Swiper('.stages__slider', {
+  direction: 'horizontal',
+  speed: 600,
+  grabCursor: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: rem(2),
+    },
+    769: {
+      slidesPerView: 4,
+      spaceBetween: rem(3),
+    },
+  },
+
+  navigation: {
+    nextEl: '.stages .next',
+    prevEl: '.stages .prev',
+  },
+})
+
+// ============  by Olesya ==============
+
+const similar_products_slider = new Swiper('.similar-products__content', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 'auto',
+
+  navigation: {
+    nextEl: '.similar-products .next',
+    prevEl: '.similar-products .prev',
+  },
+
+  autoplay: {
+    delay: 12000,
+  },
+})
